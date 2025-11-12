0b3ece8a45851ab12c016dc7e4b788d36b47a950
xaoriGit <xaorinit@gmail.com>
2025-11-12 09:55:47 +0600

    fix: исправления для сервера

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3b0f(q9l82h3frk$=!$(rzs@3opniz%r1i-lvzovpdmv5jxmqj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
